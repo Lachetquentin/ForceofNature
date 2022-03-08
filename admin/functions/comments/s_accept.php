c2c4a6039941c6f0812bf9124f0a0a13464d434f
Lachetquentin <lachet.quentin@protonmail.com>
2022-03-08 17:08:14 +0100

    feat: accept and delete comments

---
+++ admin/functions/comments/s_accept.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+?>
+
+<?php include '../../includes/config.php' ?>
+
+<?php
+
+$id = $_GET['id'];
+
+$comment_check_query = "SELECT * FROM comments_services WHERE id_comment='$id' AND published = 1 LIMIT 1";
+$result = mysqli_query($db, $comment_check_query);
+if (mysqli_num_rows($result) > 0) { // if comment exists
+
+    // Alert déjà existant
+    header("Location: ../../comments.php?erreur=1");
+    break;
+}
+// update comment if there are no errors in the form
+else {
+    // Alert réussite
+    $query = "UPDATE comments_services SET published = 1 WHERE id_comment='$id'";
+    mysqli_query($db, $query);
+    header('location: ../../comments.php?success=1');
+    break;
+}
+
+mysqli_close($db);
